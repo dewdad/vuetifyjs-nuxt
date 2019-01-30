0279d603ff364943b856909ec3b9a2f1c69a44cb
Miguel Ángel <migueabellan@gmail.com>
2019-01-30 08:51:20 +0100

    Add folder

--- pages/folder/index.vue
+++ pages/folder/index.vue
@@ -1,25 +1,24 @@
 <template>
   <v-layout>
     <v-flex>
-      <!--
-      <v-data-table :items="users">
+      <v-data-table :items="folders">
         <template slot="items" slot-scope="props">
-          <td class="text-xs-right">{{ props.item.id }}</td>
           <td>{{ props.item.name }}</td>
+          <!--
           <td class="text-xs-right">{{ props.item.username }}</td>
           <td class="text-xs-right">{{ props.item.email }}</td>
           <td class="justify-center layout px-0">
             <router-link :to="`/users/${props.item.id}`">Ver</router-link>
           </td>
+          -->
         </template>
       </v-data-table>      
-      -->
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-// import { mapState } from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
   head () {
@@ -28,32 +27,15 @@ export default {
     }
   },
   async fetch ({ store }) {
-    const folders = ['uno', 'dos']
-
-    async function asyncImport (file) {
-      let content = await import(`~/contents/${file}.md`)
-      return content.attributes
-    }
-    return Promise.all(folders.map(file => asyncImport(file)))
-      .then((res) => {
-        console.log(res)
-        return {
-          folders: res
-        }
-      })
-  }
-  /*,
-  async fetch ({ store }) {
-    await store.dispatch('user/getUsers')
-  }
-  /*,
+    await store.dispatch('folder/getFolders')
+  },
   computed: {
     ...mapState({
-      users: state => {
-        return state.user.users
+      folders: state => {
+        console.log(state.folder.folders)
+        return state.folder.folders
       }
     })
   }
-  */
 }
 </script>
--- store/folder.js
+++ store/folder.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 export const state = () => ({
   folder: {},
@@ -6,37 +6,42 @@ export const state = () => ({
 })
 
 export const mutations = {
-  /*
-  SET_FOLDER: (state, folder) => {
-    state.folder = folder
-  },
-  */
   SET_FOLDERS: (state, folders) => {
     state.folders = folders
+  },
+  ADD_FOLDER: (state, folder) => {
+    state.folders = folder
   }
 }
 
 export const actions = {
-  /*
-  async getUser ({ commit }, params) {
-    let { data } = await axios.get('https://jsonplaceholder.typicode.com/users/' + params.id)
-    commit('SET_USER', data)
-  },
-  */
   async getFolders ({ commit }) {
-    let { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
-    commit('SET_FOLDERS', data)
-  }
-  /*,
+    const folders = ['uno', 'dos']
+
+    async function asyncImport (file) {
+      let content = await import(`~/contents/${file}.md`)
+      /*
+      return {
+        attributes: content.attributes,
+        body: content.body,
+        html: content.html
+      }
+      */
+      return content.attributes
+    }
+
+    return Promise.all(folders.map(file => asyncImport(file)))
+      .then((res) => {
+        console.log(res)
+        commit('ADD_FOLDER', res)
+      })
+  },
   async nuxtServerInit ({ commit }, { route, params }) {
     if (process.server && params.id) {
-      let { data } = await axios.get('https://jsonplaceholder.typicode.com/users/' + params.id)
-      commit('SET_USER', data)
+      // ToDo
     }
-    if (process.server && route.name === 'users') {
-      let { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
-      commit('SET_USERS', data)
+    if (process.server && route.name === 'folder') {
+      // ToDo
     }
   }
-  */
 }
